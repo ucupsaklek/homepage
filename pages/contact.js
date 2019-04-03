e0e5cf6a81c119cafcfee1bd5e3907d4653b5923
allanggadong <mucktar.pakpahan@gmail.com>
2019-04-03 14:46:58 +0700

    fix contact page

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ import AccountList from '../src/components/AccountList'
 
 export default (props) => (
   <div className="contacts">
-    <div className="contacts-bg"></div>
+    <div className="head-bg"></div>
     <div className="join">
       <Container>
         <Row>
@@ -14,31 +14,31 @@ export default (props) => (
 
             <Form className="form-join">
               <FormGroup row>
-                <Label for="contact_name" sm={5} className="d-none d-sm-block">Your full name</Label>
-                <Col xs={12} sm={7}>
+                <Label for="contact_name" xs={12} sm={12} md={5}>Your full name</Label>
+                <Col xs={12} md={7}>
                   <Input type="text" name="contact_name" id="contact_name" placeholder="John Doe" />
                 </Col>
               </FormGroup>
               <FormGroup row>
-                <Label for="contact_email" sm={5} className="d-none d-sm-block">Your email address</Label>
-                <Col xs={12} sm={7}>
+                <Label for="contact_email" xs={12} sm={12} md={5}>Your email address</Label>
+                <Col xs={12} md={7}>
                   <Input type="email" name="contact_email" id="contact_email" placeholder="john.doe@email.com"/>
                 </Col>
               </FormGroup>
               <FormGroup row className="message">
-                <Label for="contact_message" sm={5} className="d-none d-sm-block">What you gonna tell us?</Label>
-                <Col xs={12} sm={7}>
+                <Label for="contact_message" xs={12}  sm={12} md={5}>What you gonna tell us?</Label>
+                <Col xs={12} md={7}>
                    <Input type="textarea" name="contact_message" />
                 </Col>
               </FormGroup>
               <FormGroup check row>
-                <Col sm={{ size: 7, offset: 5 }} className="action">
+                <Col md={{ size: 7, offset: 5 }} className="action">
                   <Button>Send</Button>
                 </Col>
               </FormGroup>
             </Form>
           </Col>
-          <Col xs={12} md={12} lg={5} className="d-none d-sm-block">
+          <Col xs={12} md={12} lg={5}>
             <div className="banner illus-message contact">
             </div>
           </Col>
--- pages/plasma-chamber.js
+++ pages/plasma-chamber.js
@@ -11,43 +11,67 @@ import Features from 'components/Product/Features'
 import UseCase from 'components/Product/UseCase'
 import Document from 'components/Product/Document'
 
-export default (props) => (
-  <div className="product">
-    <div className="product-bg"></div>
-    <MemoryRouter>
-      <Navbar
-        expand={true}
-      >
-        <Container>
-          <NavbarBrand to='/'>
-            <div className="title">
-              Plasma Chamber
-            </div>
-          </NavbarBrand>
-          <Nav navbar>
-            <NavItem active>
-              <Link to='/'>Overview</Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/features'>Features</Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/usecase'>Use Case</Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/docs'>Docs</Link>
-            </NavItem>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Container className="content">
-        <Switch>
-          <Route exact path="/" component={Overview} />
-          <Route path="/features" component={Features} />
-          <Route path="/usecase" component={UseCase} />
-          <Route path="/docs" component={Document} />
-        </Switch>
-      </Container>
-    </MemoryRouter>
-  </div>
-)
+class Product extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      open: false,
+      active: '/'
+    }
+  }
+
+  isActive = (path) => {
+    return this.state.active == path
+  }
+
+  componentDidUpdate = () => {
+    console.log(this);
+  }
+
+  render () {
+    this.isActive('/')
+    return (
+      <div className="product">
+        <div className="product-bg"></div>
+        <MemoryRouter>
+          <Navbar
+            expand={true}
+          >
+            <Container>
+              <NavbarBrand to='/'>
+                <div className="title">
+                  Plasma Chamber
+                </div>
+              </NavbarBrand>
+              <Nav navbar>
+                <NavItem active>
+                  <Link to='/'>Overview</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/features'>Features</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/usecase'>Use Case</Link>
+                </NavItem>
+                <NavItem>
+                  <Link to='/docs'>Docs</Link>
+                </NavItem>
+              </Nav>
+            </Container>
+          </Navbar>
+          <Container className="content">
+            <Switch>
+              <Route exact path="/" component={Overview} />
+              <Route path="/features" component={Features} />
+              <Route path="/usecase" component={UseCase} />
+              <Route path="/docs" component={Document} />
+            </Switch>
+          </Container>
+        </MemoryRouter>
+      </div>
+    )
+  }
+}
+
+export default Product
